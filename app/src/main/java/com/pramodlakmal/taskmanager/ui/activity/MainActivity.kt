653b4711d1f5cfe4a250762261a77fbc0f920ca3
Pramod Lakmal <pramodlakmalofficial@gmail.com>
2024-05-12 00:32:30 +0530

    complete MainActivity

--- app/src/main/java/com/pramodlakmal/taskmanager/ui/activity/MainActivity.kt
+++ app/src/main/java/com/pramodlakmal/taskmanager/ui/activity/MainActivity.kt
@@ -1,21 +1,138 @@
 package com.pramodlakmal.taskmanager.ui.activity
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.Constraints
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.pramodlakmal.taskmanager.R
+import com.pramodlakmal.taskmanager.adapter.TaskDataAdapter
+import com.pramodlakmal.taskmanager.adapter.TaskDataModel
+import com.pramodlakmal.taskmanager.databinding.ActivityMainBinding
+import com.pramodlakmal.taskmanager.db.TodoDatabase
+import com.pramodlakmal.taskmanager.model.Priority
+import com.pramodlakmal.taskmanager.model.State
+import com.pramodlakmal.taskmanager.model.Task
+import com.pramodlakmal.taskmanager.ui.MainViewModel
+import com.pramodlakmal.taskmanager.ui.MainViewModelProviderFactory
+import com.pramodlakmal.taskmanager.ui.dialog.TaskDialog
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var mainViewModel: MainViewModel
+    private var _taskDialog: TaskDialog? = null
+    private val taskDialog get() = _taskDialog!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        mainViewModel = ViewModelProvider(
+            this,
+            MainViewModelProviderFactory(TodoDatabase(this))
+        )[MainViewModel::class.java]
+
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupToolbar()
+        setupRecyclerView()
+
+        mainViewModel.getTasks().observe(this) { tasks ->
+            val tasksList = tasks
+                .sortedBy { it.taskType }
+                .groupBy { it.taskType }
+                .flatMap {
+                    listOf(
+                        TaskDataModel.Header(it.key.name),
+                        *(it.value
+                            .sortedByDescending { t -> t.id!! }
+                            .map { task ->
+                                task.toDataModel()
+                            }).toTypedArray()
+                    )
+                }
+
+            taskDataAdapter.differ.submitList(tasksList)
+        }
+    }
+
+    private fun setupToolbar() {
+        mainViewModel.getToday()
+
+        mainViewModel.today.observe(this) {
+            binding.apply {
+                tvDate.text = it.date.toString()
+                tvDay.text = it.day
+                tvMonthYear.text = it.monthYear
+            }
+        }
+
+        binding.btnNewTask.setOnClickListener {
+            showTaskDialog(null)
+        }
+    }
+
+    private val taskDataAdapter by lazy {
+        TaskDataAdapter(
+            applicationContext,
+            checkTodoListener = {
+                val priority = if (it.priority != Priority.DONE) {
+                    Priority.DONE
+                } else Priority.NORMAL
+                mainViewModel.setPriority(priority, it.id)
+            },
+            itemTodoListener = {
+                showTaskDialog(it.toTask())
+            }
+        )
+    }
+
+    private fun setupRecyclerView() {
+        binding.rvTasks.apply {
+            adapter = taskDataAdapter
+            layoutManager = LinearLayoutManager(applicationContext)
+        }
+    }
+
+    private fun showTaskDialog(task: Task?) {
+        if (_taskDialog == null) {
+            _taskDialog = TaskDialog(
+                this, task,
+                onSubmitClickListener = { s, t ->
+                    when (s) {
+                        State.ADD -> mainViewModel.addTask(t)
+                        State.UPDATE -> mainViewModel.updateTask(t)
+                    }
+                    taskDialog.dismiss()
+                    _taskDialog = null
+                },
+                onDeleteClickListener = {
+                    mainViewModel.deleteTask(it)
+                    taskDialog.dismiss()
+                    _taskDialog = null
+                }
+            )
+        }
+
+        taskDialog.show()
+
+        taskDialog.window?.apply {
+            setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            setLayout(
+                Constraints.LayoutParams.MATCH_PARENT,
+                Constraints.LayoutParams.WRAP_CONTENT
+            )
+        }
+
+        taskDialog.setOnDismissListener {
+            _taskDialog = null
         }
     }
 }
